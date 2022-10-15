44a5f9b1dc57209ee1e68cd74ffcd3b3274b1952
elleserdonaldson <ellessedon@gmail.com>
2022-10-15 16:03:31 +0100

    conditional statements such as 'if' for pass length

--- script.js
+++ script.js
@@ -18,10 +18,17 @@ function generatePassword() {
     hasSpecialChars: false,
     hasNumberChars: false,
   }
+  
+  // validate the length that the user has entered 
   var length = window.prompt("How long would you like your password to be?");
-// validate the length that the user has entered 
+// if the answer is not a number the following alert will appear 
 if (isNaN(length)) {
-  window.alert("you must enter a valid number")
+  window.alert("You must enter a valid number");
+  return;
+}
+else if (length<8 || length>128) {
+  window.alert("You must choose a password length between 8-128 characters");
+  return;
 }
 
 options.numberOfChars = length;
